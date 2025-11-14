46803a6ca6d22f1a45f61ba99a2099da7db0cfb7
gottalasahasrareddy <gottalasahasrareddy@gmail.com>
2025-11-14 21:25:00 +0530

    added code

---
+++ studentmarksservice-master/src/main/java/com/edu/vnrvjiet/sms/StudentmarkserviceApplication.java
@@ -0,0 +1,14 @@
+package com.edu.vnrvjiet.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentmarkserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentmarkserviceApplication.class, args);
+        System.out.println("Tomcat running for Student Mark Microservice...");
+	}
+
+}
